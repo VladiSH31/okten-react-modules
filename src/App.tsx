b1854fda1f2c96410804ffa1fafe2c75c0a6e88e
VladiSH31 <vladislav.sheremet@ukr.net>
2025-05-08 15:23:41 +0300

    Home Work 1 - Task 1 - #gi01MZ6v

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
-import {productsArray} from "./data/ProductsArray.tsx";
-import MyProducts from "./components/my-products/MyProducts.tsx";
+import {coursesTitleArray} from "./data/coursesTitleArray.tsx";
 
 function App() {
     return (
         <>
             {
-                productsArray.map((product, index) => <MyProducts key={index} product={product}/>
-                    )
+                coursesTitleArray.map((title: string, index) =>
+                    <div  key={index}>
+                        <h2 key={index} className="text-4xl text-cyan-800 border-2 border-solid border-cyan-400 mt-1 text-center">{title}</h2>
+                    </div>
+                )
 
             }
         </>
