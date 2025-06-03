0adf2aa3dec54b230147326e2555e4c47009023e
VladiSH31 <vladislav.sheremet@ukr.net>
2025-06-03 14:58:20 +0300

    FE. Dynamic routes CW After restored

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import MainLayout from "./layouts/MainLayout.tsx";
-import HomePage from "./pages/HomePage.tsx";
-import UsersPage from "./pages/UsersPage.tsx";
-import PostsPage from "./pages/PostsPage.tsx";
-import SingleUserDetailsPage from "./pages/SingleUserDetailsPage.tsx";
+import {RouterProvider} from "react-router-dom";
+import {routes} from "./route/routes.tsx";
 
-const routes = createBrowserRouter([
-    {path: '', element: <MainLayout/>,
-        children: [
-            {index: true, element: <HomePage/>},
-            {path: 'users', element: <UsersPage/>},
-            {path: 'users/details', element: <SingleUserDetailsPage/>},
-            {path: 'posts', element: <PostsPage/>}
-        ]}
-])
+
 
 createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>)
---
+++ src/route/routes.tsx
@@ -0,0 +1,16 @@
+import {createBrowserRouter} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout.tsx";
+import HomePage from "../pages/HomePage.tsx";
+import UsersPage from "../pages/UsersPage.tsx";
+import SingleUserDetailsPage from "../pages/SingleUserDetailsPage.tsx";
+import PostsPage from "../pages/PostsPage.tsx";
+
+export const routes = createBrowserRouter([
+    {path: '', element: <MainLayout/>,
+        children: [
+            {index: true, element: <HomePage/>},
+            {path: 'users', element: <UsersPage/>},
+            {path: 'users/details', element: <SingleUserDetailsPage/>},
+            {path: 'posts', element: <PostsPage/>}
+        ]}
+])
