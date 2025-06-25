9cc206203f68bc097101d45c8ed8680171dd512f
VladiSH31 <vladislav.sheremet@ukr.net>
2025-06-25 11:19:58 +0300

    Lesson 5. FE. useForm hook 1. React hook form instal 2. Label for input

--- src/components/form-component/FormComponent.tsx
+++ src/components/form-component/FormComponent.tsx
@@ -1,18 +1,24 @@
+import {useForm} from "react-hook-form";
 
 interface IFormProps {
     username: string,
-    password: string
+    password: string,
+    age: number
 }
 
 const FormComponent = () => {
 
-
+    useForm<IFormProps>();
 
     return (
         <div className={'m-1'}>
             <form className={''}>
-                <input className={'border-1 rounded-md m-2'} type="text" name={'username'}/>
-                <input className={'border-1 rounded-md m-2'} type="text" name={'password'}/>
+                <label htmlFor="{'username'}" className="mb-2 font-medium text-gray-700">Username:</label>
+                <input id={'username'} className={'border-1 rounded-md m-2'} type="text" name={'username'}/>
+                <label htmlFor="{'password'}" className="mb-2 font-medium text-gray-700">Password:</label>
+                <input id={'password'} className={'border-1 rounded-md m-2'} type="text" name={'password'}/>
+                <label htmlFor="{'age'}" className="mb-2 font-medium text-gray-700">Age:</label>
+                <input id={'age'} className={'border-1 rounded-md m-2'} type="number" name={'age'}/>
                 <button className={'border-1 rounded-md m-2'}>send</button>
             </form>
         </div>
