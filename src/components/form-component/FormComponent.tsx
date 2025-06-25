f68e7977cb974b2969ded3b7c6655507274b9edf
VladiSH31 <vladislav.sheremet@ukr.net>
2025-06-25 12:02:04 +0300

    Lesson 5. FE. useForm hook 1. CW Done

--- src/components/form-component/FormComponent.tsx
+++ src/components/form-component/FormComponent.tsx
@@ -8,17 +8,20 @@ interface IFormProps {
 
 const FormComponent = () => {
 
-    useForm<IFormProps>();
+    const {handleSubmit, register} = useForm<IFormProps>();
 
+    const handler = (formDataProps: IFormProps) => {
+        console.log(formDataProps);
+    };
     return (
         <div className={'m-1'}>
-            <form className={''}>
+            <form className={''} onSubmit={handleSubmit(handler)}>
                 <label htmlFor="{'username'}" className="mb-2 font-medium text-gray-700">Username:</label>
-                <input id={'username'} className={'border-1 rounded-md m-2'} type="text" name={'username'}/>
+                <input id={'username'} className={'border-1 rounded-md m-2'} type="text" {...register('username')}/>
                 <label htmlFor="{'password'}" className="mb-2 font-medium text-gray-700">Password:</label>
-                <input id={'password'} className={'border-1 rounded-md m-2'} type="text" name={'password'}/>
+                <input id={'password'} className={'border-1 rounded-md m-2'} type="text" {...register('password')}/>
                 <label htmlFor="{'age'}" className="mb-2 font-medium text-gray-700">Age:</label>
-                <input id={'age'} className={'border-1 rounded-md m-2'} type="number" name={'age'}/>
+                <input id={'age'} className={'border-1 rounded-md m-2'} type="number" {...register('age')}/>
                 <button className={'border-1 rounded-md m-2'}>send</button>
             </form>
         </div>
