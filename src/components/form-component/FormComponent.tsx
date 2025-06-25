db622ccc9831103f8881a3319c925fa4d41a812d
VladiSH31 <vladislav.sheremet@ukr.net>
2025-06-25 13:45:23 +0300

    Lesson 5. FE. useForm Basic Validation 1. CW Done

--- src/components/form-component/FormComponent.tsx
+++ src/components/form-component/FormComponent.tsx
@@ -8,7 +8,11 @@ interface IFormProps {
 
 const FormComponent = () => {
 
-    const {handleSubmit, register} = useForm<IFormProps>();
+    const {
+        handleSubmit,
+        register,
+        formState: {errors, isValid}
+    } = useForm<IFormProps>({mode: 'all'});
 
     const handler = (formDataProps: IFormProps) => {
         console.log(formDataProps);
@@ -16,13 +20,50 @@ const FormComponent = () => {
     return (
         <div className={'m-1'}>
             <form className={''} onSubmit={handleSubmit(handler)}>
-                <label htmlFor="{'username'}" className="mb-2 font-medium text-gray-700">Username:</label>
-                <input id={'username'} className={'border-1 rounded-md m-2'} type="text" {...register('username')}/>
-                <label htmlFor="{'password'}" className="mb-2 font-medium text-gray-700">Password:</label>
-                <input id={'password'} className={'border-1 rounded-md m-2'} type="text" {...register('password')}/>
-                <label htmlFor="{'age'}" className="mb-2 font-medium text-gray-700">Age:</label>
-                <input id={'age'} className={'border-1 rounded-md m-2'} type="number" {...register('age')}/>
-                <button className={'border-1 rounded-md m-2'}>send</button>
+                <label htmlFor="{'username'}" className="mb-2 font-medium text-gray-700">Username:
+                <input id={'username'} className={'border-1 rounded-md m-2'} type="text" {...register('username',{
+                    required: true,
+                    minLength: {
+                        value: 4,
+                        message: 'username is too short'
+                    },
+                    maxLength: {
+                        value: 12,
+                        message: 'username is too long'
+                    }
+                })}/>
+                    {errors.username && <div>{errors.username.message}</div>}
+                </label>
+                <label htmlFor="{'password'}" className="mb-2 font-medium text-gray-700">Password:
+                <input id={'password'} className={'border-1 rounded-md m-2'} type="text" {...register('password', {
+                    required: true,
+                    minLength: {
+                        value: 3,
+                        message: 'pass is too short'
+                    },
+                    maxLength: {
+                        value: 10,
+                        message: 'pass is too long'
+                    }
+                })}/>
+                    {errors.password && <div>{errors.password.message}</div>}
+                </label>
+                <label htmlFor="{'age'}" className="mb-2 font-medium text-gray-700">Age:
+                <input id={'age'} className={'border-1 rounded-md m-2'} type="number" {...register('age', {
+                    required:true,
+                    valueAsNumber: true,
+                    min: {
+                        value:1,
+                        message: 'age too small'
+                    },
+                    max: {
+                        value: 117,
+                        message: 'age to big'
+                    }
+                })}/>
+                    {errors.age && <div>{errors.age.message}</div>}
+                </label>
+                <button className={'border-1 rounded-md m-2'} disabled={!isValid}>send</button>
             </form>
         </div>
     );
