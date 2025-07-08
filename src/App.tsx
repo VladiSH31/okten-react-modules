3d08e7642a6f5aafa7f0a9149b9bfd4072e7d628
VladiSH31 <vladislav.sheremet@ukr.net>
2025-07-08 14:20:17 +0300

    Home Work 7 . Task #cf69cHp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
+import {useFetch} from "./hooks/useFetch.tsx";
 
 function App() {
+    const users = useFetch<{id: number, name: string}[]>('https://jsonplaceholder.typicode.com/users', [])
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <div>
+            {
+                users.map((user) => (
+                    <div key={user.id}>
+                        {user.id} : {user.name}
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default App
